f93944d3079cedffe237ab2a150c23473bdb37d9
Mateen S <ab.mateensediqui@gmail.com>
2023-04-01 23:23:50 +0430

    new changes

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -27,16 +27,18 @@ from account.views import(
     logout_view,
     login_view,
     account_view,
+    must_authenticate_view,
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
-    path('blog/', include('blog.urls', 'blog')),
-    path('register/',registration_view , name="register"),
-    path('logout/',logout_view, name='logout'),
-    path('login/',login_view, name='login'),
+    path('admin/', admin.site.urls),
     path('account/',account_view, name='account'),
+    path('blog/', include('blog.urls', 'blog')),
+    path('login/',login_view, name='login'),
+    path('logout/',logout_view, name='logout'),
+    path('must_authenticate/',must_authenticate_view , name="must_authenticate"),
+    path('register/',registration_view , name="register"),
 
      # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
